34ed1d0cdf2bdbc80c5a1bddf16e16b76ad08de7
lrr04 <ramcharanteja2907@gmail.com>
2021-03-09 10:51:34 +0530

    First

---
+++ database/migrations/2020_12_04_065818_create_teachers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('institute_id');
+            $table->string('employee_id')->nullable();
+            $table->text('subjects');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
